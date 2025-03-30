746583c20b5f056f416bb519541962819957d210
akshayshinde8 <akshinde151@gmail.com>
2025-03-30 15:41:01 +0530

    Assignment 4 Added

--- FifthApp/src/App.jsx
+++ FifthApp/src/App.jsx
@@ -1,16 +1,70 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StatusBar, SafeAreaView, Dimensions, Image, FlatList, } from "react-native";
+import { View, Text, StatusBar, SafeAreaView, Dimensions, Image, FlatList, Pressable, } from "react-native";
 import { styles } from "./styles";
 
 const App = () => {
 
+    const [cartCount, setCartCount] = useState(0);
+    const [isFavorite, setIsFavorite] = useState(false);
+    const [lastPressTime, setlastPressTime] = useState(0);
+
+    const fillHeart = "https://tse2.mm.bing.net/th?id=OIP.gEYHxSyXl3sMNJN4o3_AFAHaGl&pid=Api&P=0&h=180";
+    const emptyHeart = "https://tse1.mm.bing.net/th?id=OIP.GQEA_kLrdkQn7XNvKAj1uAHaHa&pid=Api&P=0&h=180";
+
+    const handlePress = () => {
+        const curTime = new Date().getTime();
+        const timeDiff = curTime - lastPressTime;
+        if (timeDiff < 300) {
+            setIsFavorite(!isFavorite);
+        }
+        setlastPressTime(curTime);
+    }
+
+    const handleAddToCart = () => {
+        setCartCount(cartCount + 1)
+    }
+
     return (
+
         <View style={styles.parentContainer}>
             <StatusBar style={styles.status} />
-            <Text>Hello World</Text>
+
+            <View style={styles.addToCartBadge}><Text style={styles.addToCartBadgeText}>{cartCount}</Text></View>
+            <Pressable
+                style={({ pressed }) => [styles.productCard,
+                { transform: [{ scale: pressed ? 1.05 : 1 }] },]}
+                android_ripple={{ color: 'lightgrey' }}
+                onPress={handlePress}
+            >
+                <View>
+                    <Image
+                        source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2JFyVMUGB2hCmAhFXOdCydqzgsCHd2BAzEA&s" }}
+                        style={{ aspectRatio: 1, }}
+                        resizeMode="contain"
+                    />
+                    <Text style={styles.productTitle}>Hill Station</Text>
+                    <Text style={styles.productDes}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae modi voluptates accusamus? Rem culpa libero fuga eaque, dolorem quos temporibus tempore? Unde dicta ratione corporis vero, quia minus veritatis quam!</Text>
+                    <View style={{ margin: 6 }}>
+                        <View>{
+                            isFavorite ? <Image
+                                source={{ uri: fillHeart }}
+                                style={styles.heartImage}
+                                resizeMode="contain"
+                            /> : <Image
+                                source={{ uri: emptyHeart }}
+                                style={styles.heartImage}
+                                resizeMode="contain"
+                            />
+                        }</View>
+                        <Text style={styles.productPrice}>$500</Text>
+                    </View>
+                </View>
+                <Pressable style={({ pressed }) => [styles.btn, pressed ? styles.btnPressed : styles.btnReleased]} onPress={handleAddToCart}>
+                    <Text style={styles.btnText}>Add To Cart</Text>
+                </Pressable>
+            </Pressable>
         </View>
     );
 };
 
 export default App;
-
--- FifthApp/src/styles.jsx
+++ FifthApp/src/styles.jsx
@@ -1,17 +1,18 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    parentContainer: { height: "100%" },
+    parentContainer: { backgroundColor: "white" },
     status: { color: "grey" },
-    loadingView: { flex: 1, justifyContent: "center", alignItems: "center" },
-    loadingText: { fontSize: 25 },
-    productCard: { width: "auto", padding: 10, flex: 1, backgroundColor: "#F8F8F8", margin: 10 },
-    productTitle: { fontWeight: "bold", fontSize: 16, textAlign: "center", color: "#1E3A8A" },
-    productCat: { fontWeight: "bold", fontSize: 16, color: "#4B5563" },
+    productCard: { width: "auto", padding: 10, margin: 10 },
+    productTitle: { fontWeight: "bold", fontSize: 20, textAlign: "center", color: "#1E3A8A" },
     productDes: { textAlign: "justify", color: "#374151" },
-    productRow: { justifyContent: "space-between", flexDirection: "row", margin: 5 },
-    productPrice: { fontWeight: "bold", color: "#047857" },
-    productRating: { fontWeight: "bold", color: "#FBBF24" },
-    productRating1: { fontWeight: "bold", color: "red" },
-
-})
+    btn: { borderRadius: 10, backgroundColor: "blue", alignItems: "center", padding: 10, },
+    btnText: { fontWeight: "bold", fontSize: 16, color: "#fff," },
+    btnPressed: { backgroundColor: "green" },
+    btnReleased: { backgroundColor: "#0069d9" },
+    cartText: { alignSelf: "flex-end", marginHorizontal: 10, fontSize: 20, color: "#0069d9", },
+    productPrice: { fontWeight: "bold", color: "#047857", fontSize: 20, alignSelf: "flex-end" },
+    addToCartBadge: { position: 'absolute', top: -5, right: 10, backgroundColor: '#ff0000', padding: 5, borderRadius: 10 },
+    addToCartBadgeText: { fontSize: 12, color: '#fff' },
+    heartImage:{ width: 20, height: 20, position: "absolute", left: 10 }
+});
